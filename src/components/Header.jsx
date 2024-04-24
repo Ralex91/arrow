2ece76b953ceea8cedbd8a25094d73ffd8e5912a
Ralex <95540504+Ralex91@users.noreply.github.com>
2024-04-24 10:58:12 +0200

    Add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import { PlusIcon } from "@radix-ui/react-icons"
+import Image from "next/image"
 import Link from "next/link"
 
 const Header = () => (
   <header className="border-b border-gray-200 dark:border-slate-700 sticky h-16 top-0 bg-white z-[99] px-4 dark:bg-slate-800">
     <div className="mx-auto max-w-7xl flex h-full justify-between items-center">
       <Link href="/" className=" font-extrabold">
-        LOGO
+        <Image
+          alt="logo"
+          width="300"
+          height="300"
+          src="/arrow.svg"
+          className="object-cover flex-1 w-40"
+        />
       </Link>
       <nav>
         <ul className="flex gap-4 h-full items-center">
